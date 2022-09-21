cf4ce0844b38f9fcd08cf1409f06d2ac16210824
Ubaid <khanxx777@gmail.com>
2022-09-21 19:03:39 +0500

    ubaid

--- assets/scripts/servicesDetails.js
+++ assets/scripts/servicesDetails.js
@@ -6,27 +6,29 @@ if (ElServicerowGrid) {
   ElServicerowGrid.innerHTML = "";
   for (singleserviceGrid of ourServiceDetails)
     if (singleserviceGrid.id === myService) {
-      ElServicerowGrid.innerHTML += `
-        <div class="col-10 col-sm-5 gridServices">
-                <div class="row">
-                  <div class="col-3">
-                    <img
-                      src="${singleserviceGrid.serviceCategDetails.categImg}"
-                      alt="${singleserviceGrid.categHeading}"
-                    />
-                  </div>
-                  <div class="col-9 text-left">
-                    <h3>${singleserviceGrid.categHeading}</h3>
-                  </div>
+      for (singleserviceGridDesc of singleserviceGrid.serviceCategDetails) {
+        ElServicerowGrid.innerHTML += `
+      <div class="col-10 col-sm-5 gridServices">
+              <div class="row">
+                <div class="col-3">
+                  <img
+                    src="${singleserviceGrid.serviceCategDetails.categImg}"
+                    alt="${singleserviceGrid.categHeading}"
+                  />
                 </div>
-                <div class="row">
-                  <div class="col-12">
-                    <p>
-                     ${singleserviceGrid.categDesc}
-                    </p>
-                  </div>
+                <div class="col-9 text-left">
+                  <h3>${singleserviceGrid.categHeading}</h3>
                 </div>
               </div>
-        `;
+              <div class="row">
+                <div class="col-12">
+                  <p>
+                   ${singleserviceGrid.categDesc}
+                  </p>
+                </div>
+              </div>
+            </div>
+      `;
+      }
     }
 }
